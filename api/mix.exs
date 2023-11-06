171b263599f1a38548384c433d2aadce60f1f51c
Antoine Venturini <antoine.venturini@epitech.eu>
2023-11-06 10:07:45 +0100

    Work on travis-ci and mix.exs

--- api/mix.exs
+++ api/mix.exs
@@ -5,7 +5,7 @@ defmodule TimeManager.MixProject do
     [
       app: :time_manager,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.15.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix
       ] ++ Mix.compilers(),
