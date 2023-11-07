51d506d7bc69c75e0b7ca70b5a6047f51aa7906d
BertinBintou <102294906+BertinBintou@users.noreply.github.com>
2023-11-07 15:41:25 +0100

    modal show

--- app/src/components/Dashboard.vue
+++ app/src/components/Dashboard.vue
@@ -2,7 +2,45 @@
  <div class="container">
   <div class="row">
     <div class="col-2"><UserProfile /></div>
-    <div class="col-9"><WorkingTimes/> <ModalWorkingTimeVue/> </div>
+    <div class="col-9">
+      <div class="intraContainer">
+        <WorkingTimes/> 
+        <pButton label="Submit" />
+
+
+
+
+
+        <!-- <div class="card">
+        <Button label="Show" icon="pi pi-external-link" @click="dialogVisible = true" />
+
+        <pDialog v-model:visible="dialogVisible" header="Flex Scroll" :style="{ width: '75vw' }" maximizable modal :contentStyle="{ height: '300px' }">
+            <DataTable :value="customers" scrollable scrollHeight="flex" tableStyle="min-width: 50rem">
+                <Column field="name" header="Name"></Column>
+                <Column field="country.name" header="Country"></Column>
+                <Column field="representative.name" header="Representative"></Column>
+                <Column field="company" header="Company"></Column>
+            </DataTable>
+            <template #footer>
+                <Button label="Ok" icon="pi pi-check" @click="dialogVisible = false" />
+            </template>
+        </pDialog>
+    </div> -->
+
+
+
+
+
+
+
+
+
+
+        
+        <ModalWorkingTimeVue/>
+      </div>  
+    </div>
+   
   </div>
 </div>
 
@@ -10,10 +48,13 @@
 
 
 <script>
+
 import UserProfile from './UserProfile.vue'
 import WorkingTimes from './WorkingTimes.vue'
 import ModalWorkingTimeVue from './ModalWorkingTime.vue';
+
 //import WorkingTime from './WorkingTime.vue'
+
 
 export default {
   name: 'App',
@@ -24,18 +65,36 @@ export default {
     ModalWorkingTimeVue
   }
 }
+
+
+// import { ref, onMounted } from 'vue';
+// import { CustomerService } from '@/service/CustomerService';
+
+// const customers = ref();
+// const _dialogVisible = ref(false); // Use an underscore to indicate it's intentionally unused
+
+// onMounted(() => {
+//     CustomerService.getCustomersMedium().then((data) => {
+//         customers.value = data;
+//     });
+// });
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .col-2
 {
-  border: solid 8px pink;
-  margin: 2px;
+  border: solid 1px rgb(150, 36, 36);
+  margin: 10px;
+  border-radius: 20px;
 }
 .col-9
 {
-  border: solid 8px pink;
+  border: solid 1px rgb(0, 0, 0);
+  border-radius: 20px;
+  margin: 10px;
+  padding: 10px;
  
 }
 .row
@@ -57,5 +116,9 @@ export default {
   display: flex;
   justify-content: center;
   background-color: black;
+}
+
+.intraContainer{
+  border: 1px solid black;
 }
 </style>
--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Button from "primevue/button";
+import CascadeSelect from 'primevue/cascadeselect';
+import Dialog from 'primevue/dialog';
 
-createApp(App).mount('#app')
+
+import 'primevue/resources/themes/lara-light-teal/theme.css'
+
+// createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('pButton',Button)
+app.component('pCascadeSelect',CascadeSelect)
+app.component('pDialog', Dialog);
+app.mount('#app')
+ 
