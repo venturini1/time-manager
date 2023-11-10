e2553992bf1b7a934b6f8cf939b47b45e8f1aeea
Antoine Venturini <antoine.venturini@epitech.eu>
2023-11-10 16:34:05 +0100

    delet proxy in vue.config.js

--- app/vue.config.js
+++ app/vue.config.js
@@ -3,12 +3,8 @@
 //   transpileDependencies: true
 // })
 module.exports = {
-  devServer: {
-    proxy: 'http://13.36.64.65:8080/',
-    },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/time-manager/'
     : '/',
-    proxy: 'http://13.36.64.65:8080/',
   
 }
