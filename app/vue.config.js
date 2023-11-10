204fd36d08a88eaba8758a4576a620c1fbe56435
Antoine Venturini <antoine.venturini@epitech.eu>
2023-11-10 16:29:25 +0100

    add devServer in vue.config.js

--- app/vue.config.js
+++ app/vue.config.js
@@ -3,6 +3,9 @@
 //   transpileDependencies: true
 // })
 module.exports = {
+  devServer: {
+    proxy: 'http://13.36.64.65:8080/',
+    },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/time-manager/'
     : '/'
